825948f4e0d9988864ff61a379191fb686847922
Mandresy Randrianarinjaka <mandreshope@gmail.com>
2025-02-06 15:40:19 +0300

    feat: Remove comment

--- lib/main.dart
+++ lib/main.dart
@@ -44,13 +44,9 @@ class _MyHomePageState extends State<MyHomePage> {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
           // Column is also a layout widget. It takes a list of children and
           // arranges them vertically. By default, it sizes itself to fit its
